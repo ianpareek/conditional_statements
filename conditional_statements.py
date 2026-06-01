91e93bc3c4204f24eba02448a85836d7cc633340
ianpareek <pareekian@gmail.com>
2026-06-01 17:47:32 -0400

    Add files via upload

---
+++ conditional_statements.py
@@ -0,0 +1,15 @@
+num = 3
+if num > 0:
+    print("Positive")
+elif num < 0:
+    print("Negative")
+else:
+    print("Zero")
+
+new_num = -1
+if new_num > 0:
+    print("Positive")
+elif new_num < 0:
+    print("Negative")
+else:
+    print("Zero")
